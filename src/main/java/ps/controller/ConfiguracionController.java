4e5a2a65e351eb7d189828730ca9fd080a4283e0
Andrea <andrea.verne@hotmail.com>
2023-11-01 11:59:20 -0300

    Agrego modificaciones

--- src/main/java/ps/controller/ConfiguracionController.java
+++ src/main/java/ps/controller/ConfiguracionController.java
@@ -1,21 +1,21 @@
 package ps.controller;
 
-import org.bson.types.ObjectId;
+import java.util.Date;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+
+
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import dto.requets.ErrorResponse;
-import dto.response.ConfiguracionResponse;
 import ps.model.Configuracion;
 import ps.repository.ConfiguracionRepository;
 
@@ -41,42 +41,17 @@ public class ConfiguracionController {
 		return "Un mensaje de texto.";
 	}
 
-	// Obtener todos las configuraciones
-	@GetMapping
-	public ResponseEntity<Object> obtenerTodosLasConfiguraciones() {
-		try {
-			// TODO: Pasar al service.
-			ConfiguracionResponse jr = new ConfiguracionResponse(configuracionRepository.findAll());
-			// Algun llamado al service.
-			//throw new Exception("Este es un mensaje opcional");
-			return ResponseEntity.ok(jr);
-			
-		} catch (Exception e) {
-			// Ojo con esto por que puede enviar un error de BD al front,
-			// se deberia controlar con e custom o error generico.
-			ErrorResponse er = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(er);
-		}
-	}
 
-	// Crear un nuevo configuracion
-	@PostMapping
-	public Configuracion crearJugador(@RequestBody Configuracion configuracion) {
-		return configuracionRepository.save(configuracion);
-	}
+	    @PostMapping("/")
+	    public Configuracion addConfiguracion(@RequestBody Configuracion configuracion) {
+	        return configuracionRepository.save(configuracion);
+	    }
 
-	// Actualizar un Configuracion existente por ID
-	@PutMapping("/{id}")
-	public Configuracion actualizarConfiguracion(@PathVariable ObjectId id, @RequestBody Configuracion configuracionActualizado) {
-		configuracionActualizado.setId(id);
-		return configuracionRepository.save(configuracionActualizado);
-	}
+	    // Endpoint para obtener la configuración más reciente antes de la fecha actual
+	    @GetMapping("/")
+	    public Configuracion getCurrentConfiguration() {
+	        Date currentDate = new Date(); // Obtén la fecha actual
+	        return configuracionRepository.findFirstByFechaBeforeOrderByFechaDesc(currentDate);
+	    }
+}	
 
-	// Eliminar un Configuracion por ID
-	@DeleteMapping("/{id}")
-	public void eliminarConfiguracion(@PathVariable ObjectId id) {
-		configuracionRepository.deleteById(id);
-	}
-
-	
-}
--- src/main/java/ps/repository/ConfiguracionRepository.java
+++ src/main/java/ps/repository/ConfiguracionRepository.java
@@ -1,6 +1,8 @@
 package ps.repository;
 
+import java.util.Date;
 import org.bson.types.ObjectId;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -8,6 +10,12 @@ import ps.model.Configuracion;
 
 @EnableMongoRepositories
 public interface ConfiguracionRepository extends MongoRepository< Configuracion, ObjectId> {
+	
+
+    // Consulta para obtener la configuración más reciente anterior a una fecha específica
+    Configuracion findFirstByFechaBeforeOrderByFechaDesc(Date date);
 
 
-}
+		
+	 }
+
